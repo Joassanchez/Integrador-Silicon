2962e9c3228540a99cb3445eebd90a8069b5806d
Santiago Nuñez <133709203+SantiNunez2003@users.noreply.github.com>
2023-09-17 18:49:01 -0300

    Actualizacion Login

--- src/Componentes/Login/Login.jsx
+++ src/Componentes/Login/Login.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './Login.css'
 
 export const Login = () => {
+
+    const [usuario, setUsuario] = useState("")
+    const [contraseña, setContraseña] = useState("")
+
     return (
         <body>
             <section>
@@ -9,19 +13,20 @@ export const Login = () => {
                 <form className="formulario">
                     <h1>Login</h1>
                     <a>Usuario</a>
-                    <input className="conteiner" type='text' placeholder="  Ingrese Usuario" />
+                    <input className="conteiner" type='text' placeholder="  Ingrese Usuario" 
+                        value ={usuario}
+                        onChange={e => setUsuario(e.trget.value)}
+                    />
 
                     <a>Contraseña</a>
-                    <input className="conteiner" type='password' placeholder="  Ingrese Contraseña" />
+                    <input className="conteiner" type='password' placeholder="  Ingrese Contraseña" 
+                        value ={contraseña}
+                        onChange={e => setContraseña(e.trget.value)}
+                    />
 
                     <button className="buttonIngresar" type="button" >Ingresar</button>
                 </form>
             </section>
         </body>
-
-
-
-
-
     )
 }
